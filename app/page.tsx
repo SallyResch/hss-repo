06c772a23bc45d6b3fcaabf12c99b4871e272566
Mossi <112170091+Mosssi@users.noreply.github.com>
2026-05-19 20:51:37 +0000

    add reasuabel Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Button from "../components/Button";
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
@@ -30,12 +32,20 @@ export default function Home() {
         <div>Vad är hss</div>
         <div className="bg-hss-yellow">Våra avdelningar</div>
         <div className="bg-hss-mediumblue text-white">Trygghet/Säkerhet</div>
+
+        <div className="flex gap-4">
+          <Button variant="yellow">Bli Scout</Button>
+          <Button variant="blue">Bli Ledare</Button>
+          <Button variant="outline">Läs mer</Button>
+        </div>
       </main>
+
       <footer className="flex flex-row justify-evenly border-hss-mediumblue border-2">
         <section>Kontakt section</section>
         <section>Äventyr och kompisar</section>
         <section>Soc med</section>
       </footer>
     </div>
+  );
   );
 }
