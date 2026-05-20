b40cd74185753f605b584e830f64d7af7cecb8f4
Chaitali Deore <chaitalideore@gmail.com>
2026-05-20 17:02:32 +0200

    adding hero section:mobile version

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,18 +1,45 @@
-import Image from 'next/image'
-import heroImage from '@/public/hero.jpg'
+import Image from "next/image";
+import heroImage from "@/public/hero.jpg";
+import hasselbyLogo from "@/public/hasselbylogo.png";
+import whiteLine from "@/public/whiteline.png";
 const HeroSection = () => {
   return (
-    <section >
-      HeroSectionComponent
+    <section className="relative w-full h-146.25 md:h-202.5 mt-21.25 overflow-hidden">
       <Image
         src={heroImage}
         alt="HSS Hero Image"
         fill
         priority
-        className="object-cover"
+        className="object-cover object-bottom"
+        sizes="100vw"
       />
-      </section>
-  )
-}
 
-export default HeroSection
+      <div
+        className="absolute  flex flex-col items-center pt-5 pr-10 pb-5 pl-10.75 gap-5 w-73.5 md:w-177 md:h-133.5 h-58.25 bg-[#003660E5] rounded-br-2xl rounded-bl-2xl
+                   top-38.5 left-12 "
+      >
+        
+        <span className="text-[#F4C430] font-bold text-sm md:font-semibold md:text-6xl ">Välkommen till</span>
+        <Image
+          src={hasselbyLogo}
+          alt="HasselbyLogo"
+          className="object-contain w-53.5 h-12.75 md:w-127 md:h-30.25 md:bg-opacity-1 bg-opacity-0"
+        />
+        
+
+        <Image
+          src={whiteLine}
+          alt="WhiteLine"
+          className="object-contain  w-53.5 h-12.75 md:w-125 md:h-2 bg-opacity-0"
+        />
+
+        <p className="text-xs text-[#F4C430] md:w-127 md:text-2xl  md:h-26.25 md:font-semibold  italic leading-relaxed">
+          Här väntar Äventyret. Oavsett vem du är eller var du kommer ifrån
+          finns det ett äventyr som väntar
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
