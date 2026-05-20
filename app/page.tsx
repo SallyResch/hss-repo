02874b54df819ae0fabcc91ff59586713ed77bca
Raana <raana.barimani@hyperisland.se>
2026-05-20 09:51:20 +0200

    fixed the issue

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
             <p>Navigation</p>
             <p>Navigation</p>
             <p>Navigation</p>
-            <p className="bg-hss-yellow">Button</p>
           </nav>
         </header>
       </div>
