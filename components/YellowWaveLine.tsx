ec09b0ec2724e739bfae4a5e6ad923ebf2d8d5de
SallyResch <sallyresch27@gmail.com>
2026-05-21 15:04:25 +0200

    change width and hight in yellowline component to make it responsive

--- components/YellowWaveLine.tsx
+++ components/YellowWaveLine.tsx
@@ -4,8 +4,8 @@ const YellowWaveLine = () => {
   return (
     <>
       <svg
-        width="1440"
-        height="17"
+        width="full"
+        height="full"
         viewBox="0 0 1440 17"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
