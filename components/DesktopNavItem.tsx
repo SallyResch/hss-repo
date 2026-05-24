7c44e15c0790b50fec451b76b036a52bf374cf9e
SallyResch <sallyresch27@gmail.com>
2026-05-24 12:44:09 +0200

    added button for it children exists in navitem. Button contains onClick to render the children navitems

--- components/DesktopNavItem.tsx
+++ components/DesktopNavItem.tsx
@@ -1,14 +1,22 @@
 import { NavItem } from "@/types/navigation";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 
 export default function DesktopNavItem({ item }: { item: NavItem }) {
-    const [isMenuOpen, setMenuOpen] = useState(false)
-
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const navRef = useRef<HTMLDivElement>(null);
+    const hasChildren = item.children && item.children.length > 0;
     return (
         <div>
             <Link href={item.path}>{item.label}</Link>
+            {hasChildren && (
+                <button onClick={() => setMenuOpen(prev => !prev)}>
+                    {isMenuOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                </button>
+            )}
         </div>
     )
 }
