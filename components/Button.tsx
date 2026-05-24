6b76332f447854eb4850b7bbed5210a1b132cce6
brianna <brianna@briannas-MacBook-Pro.local>
2026-05-25 00:32:21 +0200

    fix button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -13,12 +13,14 @@ const Button = ({
   ...props
 }: ButtonProps) => {
   const baseStyles =
-    "inline-flex items-center justify-center px-[14px] py-[16px] rounded-md font-bold text-sm tracking-wide transition-all duration-200 shadow-sm active:scale-95 focus:outline-none focus:ring-2 focus:ring-hss-scout-darkblue focus:ring-offset-2";
+    "inline-flex items-center justify-center px-[16px] py-[14px] rounded-[8px] font-semibold text-base md:text-[18px] transition-all duration-200 shadow-sm active:scale-90 focus:outline-none"
 
   const variantStyles = {
-    yellow: "bg-hss-yellow text-hss-mediumblue hover:opacity-90 hover:shadow-md",
-    // 修复了 border-1 为合法的 border
-    outline: "bg-hss-mediumblue border border-hss-yellow text-hss-yellow hover:border-hss-yellow/80 hover:text-hss-yellow/80",
+    yellow: "bg-hss-yellow text-hss-mediumblue hover:bg-hss-mediumblue hover:text-hss-yellow hover:ring-1 hover:ring-inset hover:ring-hss-yellow hover:opacity-90 hover:shadow-md",
+
+    outline: "bg-hss-mediumblue ring-1 ring-inset ring-hss-yellow text-hss-yellow hover:bg-hss-yellow hover:text-hss-mediumblue hover:ring-0 hover:opacity-90 hover:shadow-md",
+    
+    
     blue: "bg-hss-mediumblue text-hss-yellow hover:opacity-90 hover:shadow-md",
   };
 
