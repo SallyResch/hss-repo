55c930d463eaef3b59b9b648da9c87038a80d132
Raana <raana.barimani@hyperisland.se>
2026-05-27 15:13:51 +0200

    fixed family

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Button from "../components/Button";
 import heroimg from "../public/hero.jpg";
 import heromob from "../public/heromob.png";
 import Footer from "../components/Footer";
+import HistorySection from "@/components/HistorySection";
 
 import kidIcon from "./assets/kid.svg";
 import kiddIcon from "./assets/kidd.svg";
@@ -16,7 +17,14 @@ export default function Home() {
     <div className="flex flex-col min-h-screen box-border">
       <main className="grow">
         <div>
-          <HeroSection />
+          <HeroSection
+            title="Välkommen till"
+            description="Här väntar Äventyret. Oavsett vem du är eller var du kommer ifrån finns det ett äventyr som väntar"
+            imageUrl={heroimg}
+            mobileImgUrl={heromob}
+            showButtons={true}
+            variant="home"
+          />
         </div>
 
         <div>
@@ -71,7 +79,6 @@ export default function Home() {
         <div className="flex flex-wrap gap-4 p-6 justify-center bg-white">
           <Button variant="yellow">Bli Scout</Button>
           <Button variant="outline">Bli Ledare</Button>
-          <Button variant="blue">Anmäl intresse</Button>
           <Button variant="blue">Anmäl intresse</Button>
         </div>
       </main>
