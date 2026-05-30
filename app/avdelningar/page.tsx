91ce3a817631f5ae119b5a8c3f3f0b829d477dba
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-05-30 19:11:18 +0200

    adding departments

--- app/avdelningar/page.tsx
+++ app/avdelningar/page.tsx
@@ -1,18 +1,29 @@
-import HeroSection from '@/components/HeroSection'
-import heroimg from '../../public/Wireframe.png'
+import HeroSection from "@/components/HeroSection";
+import ScoutGroupSection from "@/app/avdelningar/Department";
 
-type Props = {}
+import heroimg from "@/public/Wireframe.png";
+import { scoutGroups } from "@/data/scoutGroups";
 
-export default function Avdelningar({ }: Props) {
-    return (
-        <div>
-            <HeroSection 
-                      title="Bli Scout" 
-                      description="Har du eller dit barn funderingar på att bli scout, nedan följer information om vad som kan förväntas av dig eller dit barn" 
-                      imageUrl={heroimg}
-                      mobileImgUrl={heroimg}
-                      showButtons={false}
-                      variant="subpage"/>
-        </div>
-    )
+export default function Avdelningar() {
+  return (
+    <>
+      <HeroSection
+        title="Bli Scout"
+        description="Har du eller dit barn funderingar på att bli scout, nedan följer information om vad som kan förväntas av dig eller dit barn"
+        imageUrl={heroimg}
+        mobileImgUrl={heroimg}
+        showButtons={false}
+        variant="subpage"
+      />
+
+      <main className="py-12">
+        {scoutGroups.map((group) => (
+          <ScoutGroupSection
+            key={group.title}
+            {...group}
+          />
+        ))}
+      </main>
+    </>
+  );
 }
