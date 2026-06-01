c229bb613a9c2952244fd42a2d02d6bf53d0cdf2
Chaitali Deore <chaitalideore@gmail.com>
2026-06-01 12:28:19 +0200

    added herosection to varaplaster page

--- app/om-hss/vara-platser/page.tsx
+++ app/om-hss/vara-platser/page.tsx
@@ -1,6 +1,16 @@
-
+import HeroSection from "@/components/HeroSection";
+import varaplasterhero from "@/public/varaplasterhero.png";
 export default function VaraPlatser() {
-    return (
-        <div>VaraPlatser</div>
-    )
+  return (
+    <div>
+      <HeroSection
+        title="Våra platser"
+        description="Ruffen och Myset är våra två sjöscoutplatser. Här håller vi möten, läger och äventyr året runt."
+        imageUrl={varaplasterhero}
+        mobileImgUrl={varaplasterhero}
+        showButtons={false}
+        variant="subpage"
+      />
+    </div>
+  );
 }
