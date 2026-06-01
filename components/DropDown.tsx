651a144a21ec092d83e1e1f7f236c48157a5c642
SallyResch <sallyresch27@gmail.com>
2026-06-01 09:16:52 +0200

    added blank pages to open when clicking on scoutnavigation links. preped for aria-current page with usePathname from next/navigation

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -1,10 +1,12 @@
 import { DropDownProps } from "@/types/dropdown"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { useState } from "react";
 export default function DropDown({ items, isOpen, className, onSelect }: DropDownProps) {
   const [openItem, setOpenItem] = useState<string | null>(null);
+  const pathname = usePathname();
   if (!isOpen) return false;
   return (
     <div className={`absolute flex flex-col gap-4 p-4 rounded shadow-lg z-20 text-hss-mediumblue ${className}`}>
@@ -25,7 +27,7 @@ export default function DropDown({ items, isOpen, className, onSelect }: DropDow
           {item.children && openItem == item.path && (
             <div className="flex flex-col gap-3">
               {item.children.map((child) => (
-                <Link key={child.path} href={child.path} onClick={onSelect}>
+                <Link key={child.path} href={child.path} onClick={onSelect} className="hover:border-b-2 border-hss-mediumblue">
                   {child.label}
                 </Link>))}
             </div>
--- components/ScoutHeader.tsx
+++ components/ScoutHeader.tsx
@@ -2,7 +2,7 @@ import ScoutNav from "@/components/ScoutNav";
 export default function ScoutHeader() {
     return (
         <header className="flex flex-row justify-between w-full pr-4 bg-hss-scout-darkblue  text-white">
-            <a href="https://www.scouterna.se/">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.scouterna.se/">
                 <img src="/scouterna-logo.svg" alt="scouternas officiella logga" className="h-12" />
             </a>
             <ScoutNav />
