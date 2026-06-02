ff0ba40ec5d8adf251a40d541124bf0809bff77e
Chaitali Deore <chaitalideore@gmail.com>
2026-06-02 21:40:13 +0200

    Fixed mobile stacking behavior and tablet image visibility

--- components/ScoutPlatserCard.tsx
+++ components/ScoutPlatserCard.tsx
@@ -1,36 +1,33 @@
 import React from "react";
 import Image from "next/image";
-import ruffenImage from "../public/ruffen.png";
 
-
-const ScoutPlatserCard = () => {
+import scoutPlatserCardsProps from "../types/scoutPlatserCardProps"
+const ScoutPlatserCard = ({imageUrl,title,description,isReverse}:scoutPlatserCardsProps) => {
+  if (!imageUrl) {
+    return null; 
+  }
   return (
-    <section className="grid grid-cols-2 md:w-[1000px] md:h-[400px] gap-[24px] mt-2 overflow-hidden rounded-2xl mb-2 bg-white border border-gray-100">
-      <div className="relative w-full h-full">
+    <section className="grid grid-cols-1 lg:grid-cols-2 lg:w-[1000px] lg:min-h-[400px] gap-[24px] mt-2 overflow-hidden rounded-2xl mb-6">
+     <div className={`relative w-full h-[250px] lg:h-full ${isReverse ? "lg:order-last" : "lg:order-first"}`}>
         <Image
-          src={ruffenImage}
+          src={imageUrl}
           alt="RuffenImage"
           fill
-          className="object-cover"
+          className="object-cover "
         />
       </div>
 
       <div className="flex flex-col justify-between py-6 pr-6 ">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col ">
           <h2 className="font-semibold text-hss-mediumblue text-[32px] ">
-            Ruffen
+            {title}
           </h2>
-          <p className="text-[16px] font-normal leading-relaxed ">
-            Ruffen ligger vid Mälaren och är vår egna stuga. Här har vi brygga,
-            båtplats och gott om utrymme för aktiviteter och läger.
+          <p className="text-[16px] font-normal mb-4 ">
+           {description}
           </p>
-        </div>
-
-        <div className="my-2">
+        
           <ul className="text-[16px] font-normal space-y-1.5 ">
-            <li className="flex items-center gap-2">
-              ✔️ Möten, läger och segling
-            </li>
+            <li className="flex items-center gap-2">✔️ Möten, läger och segling </li>
             <li className="flex items-center gap-2">✔️ Brygga och båtar</li>
             <li className="flex items-center gap-2">✔️ Kök och sovutrymmen</li>
             <li className="flex items-center gap-2">✔️ Grillplats</li>
