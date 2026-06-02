b57e319c446a67d5dfa9c7a84941a0f9e6aed512
SallyResch <sallyresch27@gmail.com>
2026-06-02 19:27:23 +0200

    moved imports to be top of page avdelningar

--- app/avdelningar/page.tsx
+++ app/avdelningar/page.tsx
@@ -1,13 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import ScoutGroupSection from "@/app/avdelningar/Department";
 import type { Metadata } from "next"
+import heroimg from "@/public/Wireframe.png";
+import { scoutGroups } from "@/data/scoutGroups";
 
 export const metadata: Metadata = {
   title: "Avdelningar",
   description: "Sida för alla våra avdelningar"
 }
-import heroimg from "@/public/Wireframe.png";
-import { scoutGroups } from "@/data/scoutGroups";
 
 export default function Avdelningar() {
   return (
