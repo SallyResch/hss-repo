ddd1320ffe87eac0383ee4db83ed11c5437bc9a4
SallyResch <sallyresch27@gmail.com>
2026-06-03 14:32:36 +0200

    added HeroSection for Members page

--- app/medlemmar/page.tsx
+++ app/medlemmar/page.tsx
@@ -1,8 +1,17 @@
+import HeroSection from "@/components/HeroSection"
+import heroImage from "@/public/IMG_0085.png"
 
 export default function Medlemmar() {
     return (
         <div>
-
+            <HeroSection
+                title="För Medlemmar"
+                description="Här hittar du praktisk information för dig som är medlem eller vårdnadshavare. Information om Scoutnet, Discord, säkerhet, dokument och våra rutiner."
+                imageUrl={heroImage}
+                mobileImgUrl={heroImage}
+                showButtons={false}
+                variant="subpage"
+            />
         </div>
     )
 }
