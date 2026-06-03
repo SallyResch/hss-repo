79676087858e947c8cb46ad4367d85510b6a37bf
mossi <jessilin007@gmail.com>
2026-06-03 20:51:41 +0200

    add activity card

--- components/BoatCard.tsx
+++ components/BoatCard.tsx
@@ -9,7 +9,8 @@ interface BoatCardProps {
 
 const BoatCard = ({title,description,imageUrl}:BoatCardProps) => {
     return(
-        <div className="flex flex-col overflow-hidden bg-white border border-blue-100 rounded-xl shadow-sm">
+        <div className=
+        "flex flex-col overflow-hidden bg-white rounded-xl shadow-sm border-2 border-transparent hover:border-hss-yellow hover:shadow-lg transition-all duration-300 cursor-pointer">
             <div className="relative w-full h-48 md:h-56 bg-gray-100">
                 <Image 
                 src={imageUrl}
