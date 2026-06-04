97ff299c4109ff4ff28fea2e6236bfa4f14e99e4
SallyResch <sallyresch27@gmail.com>
2026-06-04 10:35:02 +0200

    fixed next config and page with double h2

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.246"],
   images: {
-<<<<<<< HEAD
     remotePatterns:[
-=======
-    remotePatterns:[    
->>>>>>> feature/043-departmentspage
       {
         protocol:"https",
         hostname:"behold.pictures",
