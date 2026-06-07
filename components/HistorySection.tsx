4381bb7eb238b4cc7bbce08d7288e4d4978aa8f4
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-06-07 22:16:34 +0200

    added background image

--- components/HistorySection.tsx
+++ components/HistorySection.tsx
@@ -2,54 +2,56 @@ import Image from "next/image";
 import Link from "next/link";
 
 import mainImage from "@/public/image 16.png";
-import image4 from "@/public/image4.png";
-import image1 from "@/public/image1.png";
-import image6 from "@/public/image6.png";
-import topWave from "@/public/Wave - Blue.png";
+import image1 from "@/public/poloroid1.png";
+import image2 from "@/public/poloroid2.jpg";
+import image3 from "@/public/poloroid3.jpg";
+import image4 from "@/public/poloroid4.png";
+
 import bottomWave from "@/public/Wave - yellow.png";
+import FloatingPolaroid from "./FloatingPolaroid";
 
 export default function HistorySection() {
   return (
-    <section className="relative overflow-hidden bg-white py-16 md:py-20 lg:py-32">
-      
+    <section
+      className="relative overflow-hidden py-16 md:py-20 lg:py-12"
+      style={{
+        backgroundImage: "url('/ship-pattern.jpg')",
+        backgroundRepeat: "repeat",
+        backgroundSize: "600px",
+      }}
+    >
+      <div className="absolute inset-0 bg-white/85" />
 
       <div className="absolute bottom-0 left-0 w-full">
-        <Image
-          src={bottomWave}
-          alt="Bottom wave"
-          className="h-auto w-full"
-        />
+        <Image src={bottomWave} alt="Bottom wave" className="h-auto w-full" />
       </div>
+      <FloatingPolaroid
+        src={image1}
+        alt="Scout activity"
+        rotation="-rotate-6"
+        className="left-[60px] top-[360px]"
+      />
 
-      <div className="absolute left-2 top-[360px] hidden min-[1190px]:block lg:left-[147px] lg:top-[391px]">
-        <Image
-          src={image4}
-          alt="Floating image"
-          width={124}
-          height={122}
-          className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[122px] lg:w-[124px]"
-        />
-      </div>
+      <FloatingPolaroid
+        src={image2}
+        alt="Scout activity"
+        rotation="-rotate-12"
+        className="left-[100px] top-[20px]"
+      />
 
-      <div className="absolute right-2 top-[210px] hidden min-[1190px]:block lg:right-[140px] lg:top-[197px]">
-        <Image
-          src={image1}
-          alt="Floating image"
-          width={125}
-          height={124}
-          className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[124px] lg:w-[125px]"
-        />
-      </div>
+      <FloatingPolaroid
+        src={image3}
+        alt="Scout activity"
+        rotation="rotate-6"
+        className="right-[80px] top-[60px]"
+      />
 
-      <div className="absolute right-2 top-[500px] hidden min-[1190px]:block lg:right-[140px] lg:top-[514px]">
-        <Image
-          src={image6}
-          alt="Floating image"
-          width={124}
-          height={124}
-          className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[124px] lg:w-[124px]"
-        />
-      </div>
+      <FloatingPolaroid
+        src={image4}
+        alt="Scout activity"
+        rotation="-rotate-12"
+        className="right-[100px] top-[380px]"
+      />
 
       <div className="relative z-10 mx-auto flex max-w-3xl flex-col items-center px-5">
         <h1 className="mb-8 text-center font-montserrat text-2xl font-semibold text-hss-mediumblue md:text-[28px] lg:mb-14 lg:text-[32px]">
