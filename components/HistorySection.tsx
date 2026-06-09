874db81d634acbbf05a324ca0c1ab80d2f8dc27b
SallyResch <sallyresch27@gmail.com>
2026-06-09 16:13:12 +0200

    changed the translation for departments title and added translation in components nested in homePage

--- components/HistorySection.tsx
+++ components/HistorySection.tsx
@@ -7,12 +7,12 @@ import image1 from "@/public/image1.png";
 import image6 from "@/public/image6.png";
 import topWave from "@/public/Wave - Blue.png";
 import bottomWave from "@/public/Wave - yellow.png";
+import { useTranslations } from "next-intl";
 
 export default function HistorySection() {
+  const t = useTranslations("homePage")
   return (
     <section className="relative overflow-hidden bg-white py-16 md:py-20 lg:py-32">
-      
-
       <div className="absolute bottom-0 left-0 w-full">
         <Image
           src={bottomWave}
@@ -20,7 +20,6 @@ export default function HistorySection() {
           className="h-auto w-full"
         />
       </div>
-
       <div className="absolute left-2 top-[360px] hidden min-[1190px]:block lg:left-[147px] lg:top-[391px]">
         <Image
           src={image4}
@@ -30,7 +29,6 @@ export default function HistorySection() {
           className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[122px] lg:w-[124px]"
         />
       </div>
-
       <div className="absolute right-2 top-[210px] hidden min-[1190px]:block lg:right-[140px] lg:top-[197px]">
         <Image
           src={image1}
@@ -40,7 +38,6 @@ export default function HistorySection() {
           className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[124px] lg:w-[125px]"
         />
       </div>
-
       <div className="absolute right-2 top-[500px] hidden min-[1190px]:block lg:right-[140px] lg:top-[514px]">
         <Image
           src={image6}
@@ -50,12 +47,10 @@ export default function HistorySection() {
           className="h-[65px] w-[65px] rounded-full object-cover shadow-lg lg:h-[124px] lg:w-[124px]"
         />
       </div>
-
       <div className="relative z-10 mx-auto flex max-w-3xl flex-col items-center px-5">
         <h1 className="mb-8 text-center font-montserrat text-2xl font-semibold text-hss-mediumblue md:text-[28px] lg:mb-14 lg:text-[32px]">
-          Vad är HSS
+          {t("aboutTitle")}
         </h1>
-
         <div className="mb-8 w-full max-w-[420px] md:max-w-[480px] lg:mb-12 lg:max-w-[639px]">
           <Image
             src={mainImage}
@@ -65,25 +60,19 @@ export default function HistorySection() {
             className="rounded-bl-[20px] rounded-br-[20px] border-l border-t border-hss-mediumblue object-cover shadow-xl"
           />
         </div>
-
         <div className="max-w-[420px] space-y-5 text-hss-mediumblue md:max-w-[480px] lg:max-w-[639px] lg:space-y-8">
           <p className="font-montserrat text-base leading-relaxed sm:text-lg lg:text-[22px]">
-            Hässelby Strands Sjöscoutkår (HSS) är en traditionsrik ideell
-            ungdomsverksamhet med fokus på scouting och båtliv.
+            {t("aboutDesc1")}
           </p>
-
           <p className="font-montserrat text-base leading-relaxed sm:text-lg lg:text-[22px]">
-            Vi är religiöst och politiskt obundna, och medlem i Svenska
-            Scoutförbundet, som med ca 65.000 medlemmar är en av Sveriges
-            största ungdomsorganisationer.
+            {t("aboutDesc2")}
           </p>
-
           <div className="flex justify-end">
             <Link
               href="/om-hss"
               className="flex items-center gap-2 text-base font-medium text-hss-mediumblue transition-all duration-300 hover:gap-4 sm:text-lg lg:text-[22px]"
             >
-              Läs mer
+              {t("btnReadMore")}
               <span className="text-2xl">→</span>
             </Link>
           </div>
