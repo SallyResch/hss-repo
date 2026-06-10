532628d5f43f424a94fa4eed5d16de52341b5840
mossi <jessilin007@gmail.com>
2026-06-10 15:26:49 +0200

    Revert "change mobile button layout from row to col"
    This reverts commit 6098e34f3bd61b5e1752b819fa8abc7fea173e6e.

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,18 +1,28 @@
-export default function SectionHeader({
-    title,
-    subtitle
-}:{
-    title:string;
-    subtitle:string;
-}){
+import Image from "next/image";
+import BlueWave2 from "@/public/waves/BlueWave2.png"
+
+interface SectionHeaderProps {
+  title:string;
+  subtitle?:string;
+}
+
+export default function SectionHeader({ title, subtitle }:SectionHeaderProps){
     return(
-        <div className="mb-8">
+        <div className=" flex flex-col items-start gap-0 mb-8">
             <h2 className="text-3xl font-bold text-hss-mediumblue">           
               {title}
             </h2>
-          <p className="text-gray-600 mt-2">
-            {subtitle}
-          </p>
+            <Image
+            src={BlueWave2}
+            alt="wave"
+            className="w-[180px] h-auto object-contain"
+            />
+
+            {subtitle && (
+              <p className="text-gray-600 leading-relaxed mt-1">
+                {subtitle}
+              </p>
+            )}
         </div>
     )
 }
