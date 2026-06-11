33e5c8cf9ff4bdfc1c0b5c82e97f09bce24c1814
SallyResch <sallyresch27@gmail.com>
2026-06-11 15:11:17 +0200

    added translation to bottom Section

--- components/BottomText.tsx
+++ components/BottomText.tsx
@@ -10,20 +10,22 @@ interface BottomTextProps {
 }
 
 export default function BottomText({
-    title = "vill du segla med oss?",
-    subtitle = "Är du redo att börja ditt scoutäventyr?",
-    buttonText = "Bli scout",
+    title,
+    subtitle,
+    buttonText,
     buttonHref = "https://www.scoutnet.se/register/in/group/764"
 }: BottomTextProps) {
+    const t = useTranslations("varaBatarPage");
+    const tBtn = useTranslations("homePage")
     return (
         <section className="w-full px-6 py-12 md:px-20 md:py-16">
 
             <div className="w-full max-w-[1000px] mx-auto bg-hss-mediumblue py-6 px-6 flex flex-col items-center justify-center justify-center text-center mt-16 mb-16 rounded-xl">
                 <h2 className="text-hss-yellow text-xl md:text-3xl font-medium mb-2">
-                    {title}
+                    {title = `${t("sailWithUsTitle")}`}
                 </h2>
                 <p className="text-white text-base md:text-lg mb-6">
-                    {subtitle}
+                    {subtitle = `${t("sailWithUsText")}`}
                 </p>
 
                 <Link
@@ -32,7 +34,7 @@ export default function BottomText({
                     className="focus:outline-none"
                 >
                     <Button>
-                        {buttonText}
+                        {buttonText = `${tBtn("btnBecomeScout")}`}
                     </Button>
                 </Link>
             </div>
