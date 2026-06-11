612557a6ec8cf35a3ffe21b25e2c923c0feaceea
SallyResch <sallyresch27@gmail.com>
2026-06-11 15:57:59 +0200

    fixed svg width and hight error

--- components/YellowWaveLine.tsx
+++ components/YellowWaveLine.tsx
@@ -4,8 +4,8 @@ const YellowWaveLine = () => {
   return (
     <>
       <svg
-        width="full"
-        height="full"
+        width="100%"
+        height="100%"
         viewBox="0 0 1440 17"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
