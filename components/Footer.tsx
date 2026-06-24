ff1b5241062c85461cf39e37764936ab80182b28
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-06-24 19:56:21 +0200

    adding translation for copyright statement

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -86,7 +86,7 @@ export default function Footer() {
             </a>
           </div>
           <p className="text-xs text-hss-mediumblue mt-6">
-            © Scouterna 2026. All right reserved.
+            {t("copyright")}
           </p>
         </div>
       </div>
